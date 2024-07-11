6e6dc5fc45663901b4b8334bab0d0646c24bf669
Dominykas Kisonas <dominykas.kisonas@citadele.lv>
2024-07-11 23:26:58 +0300

    fix email sending

--- inc/send-email.php
+++ inc/send-email.php
@@ -3,13 +3,21 @@
 
 function send_email_with_attachment($attachment_path): void
 {
+    log_message("trying to send email");
     $COMPANY_NAME = "Stiklopaketai24.lt";
 
-    $to = get_option('admin_email');
+    $to = "domkisonas@gmail.com";
     $subject = 'Naujai pateikta užklausa ' . $COMPANY_NAME;
     $body = '';
     $headers = array('Content-Type: text/html; charset=UTF-8');
     $attachments = array($attachment_path);
 
-    wp_mail($to, $subject, $body, $headers, $attachments);
+    $response = wp_mail($to, $subject, $body, $headers, $attachments);
+
+    if ($response) {
+        log_message("email sent successfully");
+    } else {
+        log_message("email sending failed");
+    }
+
 }
